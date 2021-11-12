4fa6901ace6e47d0422c6ea2708fc6444a659c9b
Hussain Moosavi <mirmoh@deakin.edu.au>
2021-11-13 06:08:59 +1100

    Initial Commit

---
+++ TabbarApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabbarApp
+//
+//  Created by Hussain Moosavi on 13/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabbarApp/TabbarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarAppApp.swift
+//  TabbarApp
+//
+//  Created by Hussain Moosavi on 13/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TabbarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
